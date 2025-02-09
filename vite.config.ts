65b43f84d073c0d0c664a056716ebe6a10a1fdd5
Andre <andremendonca0504@gmail.com>
2025-02-08 23:30:11 -0300

    git hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/condo-bets", // <- Deve estar aqui, no nível raiz
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
